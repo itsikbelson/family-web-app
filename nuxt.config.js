416945b94434a1f9ae6442812c182619d4155735
itsikbelson <itsikbelson@gmail.com>
2021-06-10 19:26:35 +0300

    Support deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,6 +47,9 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    defaultAssets: {
+      icons: 'mdi'
+    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
